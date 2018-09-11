fc6b276f66fdde8f932bde0b53a4e7a2b08e7271
L1v1on <evance6@uncc.edu>
2018-09-11 16:11:38 -0400

    playing with reference variables.

---
+++ 2018/C++TonyGaddis/Ch6/referenceVar.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+void numChange(int&);
+
+int main()
+{
+	int num = 0;
+	cout << "main() local num is: " << num << endl;
+	numChange(num);
+	cout << "main() local num is now: " << num << endl;
+	
+	return 0;
+}
+
+void numChange(int &num)
+{
+	cout << "numChange() will now change main() local num\n"; 
+	num = 23;
+}

f9f42ed18b07748869e89685e1f58667f6613e6a
evance6 <evance6@uncc.edu>
2019-03-03 17:11:28 -0500

    New workflow using code runner. Hit play button to build & run code.

--- 2018_2019/C++TonyGaddis/Ch7/arrayFunctions.cpp
+++ 2018_2019/C++TonyGaddis/Ch7/arrayFunctions.cpp
@@ -1,25 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void arrayF(int[]);
 
 int main()
 {
-	int arr[5] = {5,6,7,8,9};
+	int arr[5] = {5, 6, 7, 8, 9};
 	cout << "Display elements of array." << endl;
-	for(int x = 0; x < 5; x++)
+	for (int x = 0; x < 5; x++)
 		cout << arr[x] << " ";
 	cout << endl;
 	cout << "Call arrayF() to pass by ref arr." << endl;
 	arrayF(arr);
 	cout << "Display new elements of arr." << endl;
-	for(int x = 0; x < 5; x++)
+	for (int x = 0; x < 5; x++)
 		cout << arr[x] << " ";
 	cout << endl;
+	return 0;
 }
 
 void arrayF(int arr[])
 {
-	for(int x = 0; x < 5; x++)
+	for (int x = 0; x < 5; x++)
 		arr[x]++;
 }
--- 2018_2019/C++TonyGaddis/Ch7/arraysFiles.cpp
+++ 2018_2019/C++TonyGaddis/Ch7/arraysFiles.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -9,33 +9,33 @@ int main()
 	int num[ARR_SIZE];
 	ifstream inFile;
 	ofstream outFile;
-	
-	//Make sure the path is correct or you will get garbage data.
-	inFile.open("C:\\Users\\Livion\\Desktop\\Testing_Learning\\2018\\C++TonyGaddis\\Ch7\\testInput.txt");
-	
+
+	//Make sure the path is correct or you will get garbage data. On laptop change 3zr4 to Livion.
+	inFile.open("C:\\Users\\3zr4\\Desktop\\Testing-Learning\\2018_2019\\C++TonyGaddis\\Ch7\\testInput.txt");
+
 	cout << "Reading data from file.\n";
-	
-	for(int c = 0; c < ARR_SIZE; c++)
+
+	for (int c = 0; c < ARR_SIZE; c++)
 		inFile >> num[c];
-		
+
 	inFile.close();
-	
+
 	cout << "Displaying data.\n";
-	
-	for(int c = 0; c < ARR_SIZE; c++)
+
+	for (int c = 0; c < ARR_SIZE; c++)
 		cout << num[c] << " ";
-	
+
 	cout << "\nUpdating data.\n";
-	
-	for(int c = 0; c < ARR_SIZE; c++)
+
+	for (int c = 0; c < ARR_SIZE; c++)
 		num[c] = num[c] + 1;
-	
-	outFile.open("C:\\Users\\Livion\\Desktop\\Testing_Learning\\2018\\C++TonyGaddis\\Ch7\\testOutput.txt");
-	
+
+	outFile.open("C:\\Users\\3zr4\\Desktop\\Testing-Learning\\2018_2019\\C++TonyGaddis\\Ch7\\testOutput.txt");
+
 	cout << "Saving updated data to another file.\n";
-	
-	for(int c = 0; c < ARR_SIZE; c++)
+
+	for (int c = 0; c < ARR_SIZE; c++)
 		outFile << num[c] << endl;
-	
+
 	return 0;
 }
